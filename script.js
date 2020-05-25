a5fd9abd247d424851b21fc48aeb8df74ff57268
Ghezlene Doballah <ghyzounette@gmail.com>
2020-05-25 12:21:27 +0200

    commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function check() {
+    if (document.getElementById('amount-4').checked) {
+        document.getElementById('other-amount').style.visibility = 'visible';
+    }
+    else document.getElementById('other-amount').style.visibility = 'hidden';
+
+}
